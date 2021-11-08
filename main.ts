1b6bfe3a2e15af47eb69115937175bc77c41b483
Minab1 <92942758+Minab1@users.noreply.github.com>
2021-11-08 17:49:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(":) happy")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(":| meh")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString(":( sad")
+})
